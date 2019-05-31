22efee56ed96ac18f33247b8bd6f81fbbfc59302
Eric White <eric.white.austin@gmail.com>
2019-05-31 15:19:54 -0500

    Switched to Princeton Wordnet dictionary. Changed server and part of speech to user input.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # WoW_Names
-Program to check common words that are potentially available as a name on a WoW server using the Pearson dictionary API.
+Program to check common words that are potentially available as a name on a WoW server using the Princeton WordNet dictionary with the MIT Java Wordnet Interface (JWI).
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,68 +1,101 @@
 package main;
 
-import com.google.gson.Gson;
+import com.google.common.collect.Streams;
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+import edu.mit.jwi.item.IIndexWord;
+import edu.mit.jwi.item.POS;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.UrlValidator;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
 
 public class Main {
-    private static String DICTIONARY_API_URL = "http://api.pearson.com/v2/dictionaries/entries?part_of_speech=";
-    private static String PART_OF_SPEECH = "noun";
-    private static String WOW_API_URL = "https://worldofwarcraft.com/en-us/character/";
-    private static String WOW_SERVER = "illidan";
-    private static HashSet<String> potentialNames = new HashSet<String>();
+    private static final String DICTIONARY_PATH = "src/main/resources/dict";
+    private static final String WOW_API_URL = "https://worldofwarcraft.com/en-us/character/";
+    public static final String SERVER_PROMPT = "Server name (e.g. bleeding-hollow): ";
+    public static final String PART_OF_SPEECH_PROMPT = "Part of speech (noun, verb, adjective, adverb): ";
 
     public static void main(String[] args) throws IOException {
-        for (int i = 0; i < 2000; i++) {
-            getPotentialAvailableNames(getWords(i));
-        }
-
-    }
-    private static Words getWords(int offset) throws IOException {
-
-        offset = offset * 25;
-        String OFFSET_DICTIONARY_API_URL = DICTIONARY_API_URL + PART_OF_SPEECH + "&offset=" + offset + "&limit=25";
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(OFFSET_DICTIONARY_API_URL);
-        CloseableHttpResponse response1 = httpclient.execute(httpGet);
-        HttpEntity httpEntity = response1.getEntity();
-        String responseString = EntityUtils.toString(httpEntity, "UTF-8");
-
-        Gson gson = new Gson();
-        Words words = gson.fromJson(responseString, Words.class);
-
-        return words;
+        getNames();
     }
 
-    private static void getPotentialAvailableNames(Words words) throws IOException {
-        for (Words.Result result : words.getResults()) {
-            String word = result.getHeadword();
-            if (!potentialNames.contains(word) && isAlpha(word) && word.length() < 10) {
-                String armoryUrl = WOW_API_URL + WOW_SERVER + "/" + word;
-                UrlValidator urlValidator = new UrlValidator();
-                if (urlValidator.isValid(armoryUrl)) {
-                    CloseableHttpClient httpclient = HttpClients.createDefault();
-                    HttpGet httpGet = new HttpGet(armoryUrl);
-                    CloseableHttpResponse armoryResponse = httpclient.execute(httpGet);
+    private static void getNames() throws IOException {
+        File file = new File(DICTIONARY_PATH);
+        IDictionary dict = new Dictionary(file);
+        dict.open();
 
-                    //404 Response means the name is potentially available
-                    if (armoryResponse.getStatusLine().getStatusCode() == 404) {
-                        potentialNames.add(word);
-                        System.out.println(word);
-                    }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print(SERVER_PROMPT);
+        String serverName = scan.next();
+        System.out.print(PART_OF_SPEECH_PROMPT);
+        String partOfSpeechString = scan.next().toUpperCase();
+
+        if (validPartOfSpeech(partOfSpeechString)) {
+            POS partOfSpeech = POS.valueOf(partOfSpeechString);
+            Iterator<IIndexWord> wordIterator = dict.getIndexWordIterator(partOfSpeech);
+
+            Streams.stream(wordIterator).forEach(word -> {
+                if (nameIsPotentiallyAvailable(serverName, word.getLemma())) {
+                    System.out.println(word.getLemma());
                 }
+            });
+        } else {
+            System.out.println("Invalid part of speech: " + partOfSpeechString);
+        }
+    }
+
+    /**
+     * Check if a name is potentially available for given server. A name is considered potentially if it consists of
+     * only letters, is between 2 and 12 characters long, and returns a 404 from the WoW API.
+     *
+     * @param serverName
+     * @param name
+     * @return
+     * @throws IOException
+     */
+    private static boolean nameIsPotentiallyAvailable(String serverName, String name) {
+        String armoryUrl = WOW_API_URL + serverName + "/" + name;
+        UrlValidator urlValidator = new UrlValidator();
+        if (StringUtils.isAlpha(name) && name.length() > 1 && name.length() < 13 && urlValidator.isValid(armoryUrl)) {
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(armoryUrl);
+            CloseableHttpResponse armoryResponse = null;
+            try {
+                armoryResponse = httpclient.execute(httpGet);
+            } catch (IOException e) {
+                System.out.println("Failed to call WoW API");
+                e.printStackTrace();
+                return false;
+            }
+
+            //404 Response means the name is potentially available
+            if (armoryResponse.getStatusLine().getStatusCode() == 404) {
+                return true;
             }
         }
+        return false;
     }
 
-    public static boolean isAlpha(String name) {
-        return name.matches("[a-zA-Z]+");
+    /**
+     * Validate part of speech
+     * @param partOfSpeechString
+     * @return
+     */
+    private static boolean validPartOfSpeech(String partOfSpeechString) {
+        try {
+            POS.valueOf(partOfSpeechString);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
     }
 }
